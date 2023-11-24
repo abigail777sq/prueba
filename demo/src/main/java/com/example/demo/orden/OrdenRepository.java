293acaa8c430813472a35b486ca5d68c1e61f665
Abigail <abigailsalcedo798@gmail.com>
2023-11-23 19:52:52 -0500

    uu

--- demo/src/main/java/com/example/demo/orden/OrdenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.orden;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface  OrdenRepository  extends JpaRepository<Orden, Long> {
-    
-}
-
--- demo/src/main/java/com/example/demo/usuario/domain/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.usuario.domain;
-
-public enum Role {
-    USER,
-    ADMIN
-}
